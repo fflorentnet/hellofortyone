74fb40b7ba251926eabc4cf64dfdd22efa2a260b
TrollMcQueen <mastercloudz@gmail.com>
2013-12-07 21:36:10 +0100

    modified:   composant.c 	modified:   composant.h 	modified:   robot.c

--- composant.c
+++ composant.c
@@ -1,31 +1,3 @@
 #include <composant.h>
 //pipi
-enum COMPOSANT_TYPE {
-	C1,
-	C2,
-	C3,
-	C4
-};
-enum OPERATION {
-	INIT,
-	OP1,
-	OP2,
-	OP3,
-	OP4,
-	OP5,
-	OP6,
-	FINI
-};
-enum PRODUIT_TYPE {
-	P1,
-	P2,
-	P3,
-	P4
-};
-struct COMPOSANT {
-	COMPOSANT_TYPE type;
-};
-struct PRODUIT {
-	PRODUIT_TYPE type;
-	OPERATION op;
-};
+
---
+++ composant.h
@@ -0,0 +1,34 @@
+#ifndef PRODUIT_H
+#define PRODUIT_H
+
+enum COMPOSANT_TYPE {
+	C1,
+	C2,
+	C3,
+	C4
+};
+enum OPERATION {
+	INIT,
+	OP1,
+	OP2,
+	OP3,
+	OP4,
+	OP5,
+	OP6,
+	FINI
+};
+enum PRODUIT_TYPE {
+	P1,
+	P2,
+	P3,
+	P4
+};
+struct COMPOSANT {
+	COMPOSANT_TYPE type;
+};
+struct PRODUIT {
+	PRODUIT_TYPE type;
+	OPERATION op;
+};
+
+#endif
