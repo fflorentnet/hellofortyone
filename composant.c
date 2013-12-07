4f1a840ff1b580678bc94de91e1f12a1fe182a3e
TrollMcQueen <mastercloudz@gmail.com>
2013-12-07 18:32:50 +0100

    modified:   composant.c

--- composant.c
+++ composant.c
@@ -6,3 +6,26 @@ enum COMPOSANT_TYPE {
 	C3,
 	C4
 };
+enum OPERATION {
+	INIT,
+	OP1,
+	OP2,
+	OP3,
+	OP4,
+	OP5,
+	OP6,
+	FINI
+};
+enum PRODUIT_TYPE {
+	P1,
+	P2,
+	P3,
+	P4
+};
+struct COMPOSANT {
+	COMPOSANT_TYPE type;
+};
+struct PRODUIT {
+	PRODUIT_TYPE type;
+	OPERATION op;
+};
