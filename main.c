af108d1a821185b272dab82da57c90983d4268fc
TrollMcQueen <mastercloudz@gmail.com>
2013-12-08 23:26:18 +0100

    Resolution du bug qui empechait le lancement du thread + elaboration de test

--- main.c
+++ main.c
@@ -6,33 +6,35 @@
 
 #include "composant.h"
 #include "robot.h"
-
-
 #define NBROBOT 6
 
-void createThread(pthread_t t, ROBOT r) {
+void createThread(pthread_t t, ROBOT* r) {
+	int val;
 
-	pthread_attr_t attr; // thread attribute
-	if (pthread_attr_init (&attr) != 0) {
-		fprintf (stdout, "pthread_attr_init error");
-		exit (1);
-	}
-	if (pthread_create(&t, &attr, initRobot, (void*)&r) != 0) {
+	if ((val = pthread_create(&t, 0, initRobot, (void*) r)) != 0) {
 		printf ("pthread_create error\n");
 		exit (1);
 	}
+	pthread_join(t,NULL);
 }
 
 int main() {
 
 	// set thread detachstate attribute to DETACHED 
-
-//	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+setbuf(stdout,NULL);
+setbuf(stderr,NULL);
+	pthread_t robot[NBROBOT];
+	ROBOT r[NBROBOT];
+	r[0].op = OP1;
+	r[1].op = OP2;
+	r[2].op = OP3;
+	r[3].op = OP4;
+	r[4].op = OP5;
+	r[5].op = OP6;
 	
-pthread_t robot;
-	ROBOT r;
-	r.op = OP1;
-	createThread(robot, r);
-	return (EXIT_SUCCESS);
+	int i = 0;
+	for (i = 0;i<NBROBOT;i++)
+		createThread(robot[i], &r[i]);
+	exit (EXIT_SUCCESS);
 }
 
--- robot.c
+++ robot.c
@@ -1,14 +1,21 @@
 #include "robot.h"
 #include "composant.h"
+#include <stdio.h>
+#include <stdlib.h>
 void* initRobot(void* r)
 {
-	printf("test");
+	fflush(stdout);
 	switch( ((ROBOT*)r)->op)
 	{
-		case OP1: printf("Je fais l'operation\n");
-			break;
+		case OP1: printf("Je fais l'operation 1\n"); break;
+		case OP2: printf("Je fais l'operation 2\n"); break;
+		case OP3: printf("Je fais l'operation 3\n"); break;
+		case OP4: printf("Je fais l'operation 4\n"); break;
+		case OP5: printf("Je fais l'operation 5\n"); break;
+		case OP6: printf("Je fais l'operation 6\n"); break;
+
 		default: printf("Je n'ai pas d'opération :(\n"); break;
 	}
-	fflush(stdout);
-	pthread_exit(NULL);
+	pthread_exit(EXIT_SUCCESS);
 }
+
