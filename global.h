a824299cf1c05c6ad667ce0252cb71f371a536d6
Ecchinigami <james.wolf2712@gmail.com>
2013-12-15 00:35:25 +0100

    modified:   global.h

--- global.h
+++ global.h
@@ -1,8 +1,11 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
+#define TAILLEANNEAU 16
+
 /* Variables globales */
 volatile pthread_mutex_t mutex;
 volatile int nbRobotOK;
+volatile CASE[TAILLEANNEAU] tapis;
 
 #endif
